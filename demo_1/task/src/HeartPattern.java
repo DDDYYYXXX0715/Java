d090b547dac5390838831bb4bff277acb35cd3d7
DDDYYYXXX0715 <1652599880@qq.com>
2025-09-11 10:29:38 +0800

    test

---
+++ demo_1/task/src/HeartPattern.java
@@ -0,0 +1,15 @@
+public class HeartPattern {
+    public static void main(String[] args) {
+        // 打印爱心图案
+        System.out.println("    ❤❤         ❤❤    ");
+        System.out.println("  ❤❤❤❤❤   ❤❤❤❤❤  ");
+        System.out.println(" ❤❤❤❤❤❤❤❤❤❤❤❤❤");
+        System.out.println(" ❤❤❤❤❤❤❤❤❤❤❤❤");
+        System.out.println("  ❤❤❤❤❤❤❤❤❤❤❤  ");
+        System.out.println("    ❤❤❤❤❤❤❤❤❤    ");
+        System.out.println("      ❤❤❤❤❤❤❤      ");
+        System.out.println("        ❤❤❤❤❤       ");
+        System.out.println("          ❤❤❤         ");
+        System.out.println("            ❤        ");
+    }
+}
---
+++ 第一次实验/src/Character.java
@@ -0,0 +1,7 @@
+public class Character {
+    public static void main(String[] args) {
+        char ch;
+        System.out.println("请输入一行字符：");
+
+    }
+}
