f1691bc3140547a3aae76b945191f422927f05b2
DDDYYYXXX0715 <1652599880@qq.com>
2025-10-06 23:34:13 +0800

    change

--- src/experiment/experiment_1/HelloWorld.java
+++ src/experiment/experiment_1/HelloWorld.java
@@ -1,2 +1,7 @@
-package experiment.experiment_1;public class HelloWorld {
+package experiment.experiment_1;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
 }
